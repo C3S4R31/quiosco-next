a82ec9faf7b6ff8909befbb6d0866ff11a004159
Cesar Avila <ing.cesaravilat@gmail.com>
2025-07-23 11:55:28 -0500

    creando el store de zustand y componentes para mostrar los detalles de order

---
+++ components/order/ProductDetails.tsx
@@ -0,0 +1,11 @@
+import { OrderItem } from "@/src/types"
+
+type ProductDetailsProps = {
+    item: OrderItem
+}
+
+export default function ProductDetails({item}:ProductDetailsProps) {
+  return (
+    <div>ProductDetails</div>
+  )
+}
