daae51599b44f788585601009e7c26a3918a0772
Cesar Avila <ing.cesaravilat@gmail.com>
2025-07-28 14:59:12 -0500

    creando formulario del pedido

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,12 +3,17 @@ import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
 import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
 
 
 export default function OrderSummary() {
 
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0),[order])
+
+  const handleCreateOrder = (formData : FormData) => {
+    createOrder()
+  }
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -27,7 +32,28 @@ export default function OrderSummary() {
             <p className="text-2xl mt-20 text-center">
               Total a pagar: {''}
               <span className="font-bold">{formatCurrency(total)}</span>
-            </p>          
+            </p>   
+
+            <form 
+              className="w-full mt-10 space-y-5"
+              action={handleCreateOrder}
+            >
+
+              <input 
+                type="text"
+                placeholder="Tu Nombre"
+                className="bg-white border border-gray-100 p-2 w-full"
+                name="name"
+              />
+
+              <input
+                type="submit" 
+                className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                value='Confirmar Pedido'
+              />
+
+            </form>
+                   
           </div>
         )}
     </aside>
